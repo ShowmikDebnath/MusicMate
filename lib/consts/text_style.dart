2849a15f3bdaa12236af5036fb4c89d0acedcd38
Showmik <showmik@Doriks-MacBook-Air.local>
2025-02-02 00:37:22 +0600

    Setup related error fixed - almost fresh code

--- lib/consts/text_style.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:musicmate/consts/colors.dart';
-
-ourStyle({double? size = 14, color = whiteColor}){
-  return TextStyle(
-    fontSize: size,
-    color: color,
-  );
-}
